83d30084099448cfcc42753864f8fa8c94e3a108
JO YUN HO <yujo.dev@gmail.com>
2023-12-28 21:50:43 +0900

    입력된 값이 세 자리가 아닌 경우에 대한 검증 로직 및 테스트 추가

--- src/main/scala/vo/UserInputs.scala
+++ src/main/scala/vo/UserInputs.scala
@@ -1,7 +1,11 @@
 package vo
 
 // VO = Value Object
-class UserInput(val value: String) {
+class UserInputs(val value: String) {
   require(value != null, "값을 반드시 입력해야 합니다.")
   require(!value.isBlank, "빈 문자열을 입력할 수 없습니다.")
+  require(value.length == 3, "세 자리 숫자만 입력 가능합니다.")
+
+
+
 }
--- src/test/scala/vo/UserInputsTest.scala
+++ src/test/scala/vo/UserInputsTest.scala
@@ -2,12 +2,12 @@ package vo
 
 import org.scalatest.funsuite.AnyFunSuiteLike
 
-class UserInputTest extends AnyFunSuiteLike {
+class UserInputsTest extends AnyFunSuiteLike {
   test("생성자도 잘 몰라서 클래스 잘 생성됐는지 확인함") {
     val inputValue = "3"
     val expected = "3"
 
-    val userInput = new UserInput(inputValue)
+    val userInput = new UserInputs(inputValue)
     assert(userInput.value == expected)
   }
 
@@ -15,7 +15,7 @@ class UserInputTest extends AnyFunSuiteLike {
     val invalidValue = null
 
     val exception = intercept[IllegalArgumentException] {
-      new UserInput(invalidValue)
+      new UserInputs(invalidValue)
     }
 
     assert(exception.getMessage.contains("값을 반드시 입력해야 합니다."))
@@ -25,9 +25,19 @@ class UserInputTest extends AnyFunSuiteLike {
     val invalidValue = "       "
 
     val exception = intercept[IllegalArgumentException] {
-      new UserInput(invalidValue)
+      new UserInputs(invalidValue)
     }
 
     assert(exception.getMessage.contains("빈 문자열을 입력할 수 없습니다."))
+  }
+
+  test("유저입력값은 세 글자여야만 한다.") {
+    val invalidValue = "1234"
+
+    val exception = intercept[IllegalArgumentException] {
+      new UserInputs(invalidValue)
+    }
+
+    assert(exception.getMessage.contains("세 자리 숫자만 입력 가능합니다."))
   }
 }
