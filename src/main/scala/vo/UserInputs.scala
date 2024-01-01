6152de54db9163aa34046221a2ff4d90a3fe273e
poppo <poppo@woowahan.com>
2024-01-01 23:49:50 +0900

    불필요한 공백 제거

--- src/main/scala/vo/UserInputs.scala
+++ src/main/scala/vo/UserInputs.scala
@@ -5,7 +5,4 @@ class UserInputs(val value: String) {
   require(value != null, "값을 반드시 입력해야 합니다.")
   require(!value.isBlank, "빈 문자열을 입력할 수 없습니다.")
   require(value.length == 3, "세 자리 숫자만 입력 가능합니다.")
-
-
-
 }
