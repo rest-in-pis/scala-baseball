5e7ea05930c4e753640a63c2b37143e061ed0a6a
JO YUN HO <yujo.dev@gmail.com>
2024-01-09 22:53:12 +0900

    테스트 코드 모두 리팩터링 완료

--- src/test/scala/vo/UserInputsTest.scala
+++ src/test/scala/vo/UserInputsTest.scala
@@ -17,7 +17,10 @@ class UserInputsTest extends AnyFunSuiteLike {
     (null, classOf[IllegalArgumentException], "값을 반드시 입력해야 합니다."),
     ("       ", classOf[IllegalArgumentException], "빈 문자열을 입력할 수 없습니다."),
     ("1234", classOf[IllegalArgumentException], "세 자리만 입력 가능합니다."),
-    ("332", classOf[IllegalArgumentException], "자리수 별로 중복된 값이 있는 경우 예외를 발생시킨다.")
+    ("332", classOf[IllegalArgumentException], "중복된 숫자가 없어야 합니다."),
+    ("032", classOf[IllegalArgumentException], "0은 입력값이 될 수 없습니다."),
+    ("a01", classOf[IllegalArgumentException], "숫자만 입력 가능합니다."),
+
   )
 
   forEvery(invalidCases) {(invalidValue, expectedException, expectedMessage) =>
@@ -29,25 +32,5 @@ class UserInputsTest extends AnyFunSuiteLike {
       assert(exception.getClass == expectedException)
       assert(exception.getMessage.contains(expectedMessage))
     }
-  }
-
-  test("0이 포함된 경우 예외를 발생시킨다.") {
-    val invalidValue = "032"
-
-    val exception = intercept[IllegalArgumentException] {
-      new UserInputs(invalidValue)
-    }
-
-    assert(exception.getMessage.contains("0은 입력값이 될 수 없습니다."))
-  }
-
-  test("숫자가 아닌 문자열 세자리 입력 시 예외를 발생시킨다.") {
-    val invalidValue = "a01"
-
-    val exception = intercept[IllegalArgumentException] {
-      new UserInputs(invalidValue)
-    }
-
-    assert(exception.getMessage.contains("숫자만 입력 가능합니다."))
   }
 }
